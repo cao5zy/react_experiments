a8bed4b063d25cdda20797166d3bb21c6a787c58
zongying_cao <zongying_cao@163.com>
2019-03-28 10:01:01 +0800

    add state
    Signed-off-by: zongying_cao <zongying_cao@163.com>

--- hello-world/src/EditBox.js
+++ hello-world/src/EditBox.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 class EditBox extends Component {
-    
+    constructor(props) {
+	super(props);
+	this.state = { value: 'hello world'};
+    }    
     render() {
+	const val = this.state.value;
 	return (
 	    <div>
 		<label>Hello</label>
-		<input type='text'></input>
+		<input type='text' value={val}></input>
             </div>
-		
 	);
   }
 }
